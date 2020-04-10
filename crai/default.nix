63a5bebb86577755906065e622941aa81c2228cb
Chloé <rightfold+ck@gmail.com>
2020-04-10 20:18:15 +0200

    Read dependencies from META6.json

--- crai/default.nix
+++ crai/default.nix
@@ -1,13 +1,17 @@
 { perl, raku, rakudo, sqlite }:
-raku.rakuPackage {
-    name = "crai";
-    src = ./.;
-    depends = [ raku.DBIish ];
-    postInstallPhase = ''
-        makeWrapper ${perl}/bin/prove $out/bin/crai.prove       \
-            --set PERL6LIB "$(< $out/PERL6LIB)"                 \
-            --set LD_LIBRARY_PATH ${sqlite.out}/lib             \
-            --add-flags --exec                                  \
-            --add-flags ${rakudo}/bin/raku
-    '';
-}
+let
+    meta6 = builtins.fromJSON (builtins.readFile ./META6.json);
+    get-depend = p: raku."${builtins.replaceStrings ["::"] ["-"] p}";
+in
+    raku.rakuPackage {
+        name = "crai";
+        src = ./.;
+        depends = map get-depend meta6.depends;
+        postInstallPhase = ''
+            makeWrapper ${perl}/bin/prove $out/bin/crai.prove       \
+                --set PERL6LIB "$(< $out/PERL6LIB)"                 \
+                --set LD_LIBRARY_PATH ${sqlite.out}/lib             \
+                --add-flags --exec                                  \
+                --add-flags ${rakudo}/bin/raku
+        '';
+    }
